e9eaec1908f3a32cdd37217d7014aedccdd2b9af
Donny <doniwahyu14@gmail.com>
2023-03-23 11:33:13 +0700

    add env file

--- src/utils/octokit.tsx
+++ src/utils/octokit.tsx
@@ -1,7 +1,7 @@
 import { Octokit } from "octokit";
 
 const octokit = new Octokit({
-  auth: "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",
+  auth: process.env.token,
 });
 
 export default octokit;
